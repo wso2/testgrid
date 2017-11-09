a6364e05e5ab47c15ef687d9b57791aeff99dc66
Vidura Nanayakkara <vidura_nanayakkara@hotmail.com>
2017-11-09 21:15:01 +0530

    Fix test report generation errors

--- reporting/src/main/java/org/wso2/carbon/testgrid/reporting/reader/ResultReaderFactory.java
+++ reporting/src/main/java/org/wso2/carbon/testgrid/reporting/reader/ResultReaderFactory.java
@@ -22,6 +22,7 @@ import org.wso2.carbon.testgrid.reporting.ReportingException;
 import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 /**
  * Factory class to return an instance of a result reader based on the file extension.
@@ -41,7 +42,7 @@ public class ResultReaderFactory {
      * @throws ReportingException thrown when the path is null or if the path do not exists or if the file extension
      *                            is not supported
      */
-    public static ResultReadable getResultReader(Path path) throws ReportingException {
+    public static Optional<ResultReadable> getResultReader(Path path) throws ReportingException {
         // Check whether configuration filepath is null. proceed if not null.
         if (path == null || !path.toFile().exists()) {
             throw new ReportingException("No result file path is provided");
@@ -49,14 +50,14 @@ public class ResultReaderFactory {
 
         // Initialize result reader based on the file's extension.
         if (path.toString().endsWith(CSV_EXTENSION)) {
-            return new CSVResultReader();
+            return Optional.of(new CSVResultReader());
         } else if (path.toString().endsWith(XML_EXTENSION)) {
             // TODO: Implement a way to handle reader arguments
             Map<String, Object> args = new HashMap<>();
             args.put(XMLResultReader.XML_START_ELEMENT_NAME, "test-method");
-            return new XMLResultReader(args);
+            return Optional.of(new XMLResultReader(args));
         } else {
-            throw new ReportingException("Error while initializing result reader, file extension is not supported");
+            return Optional.empty();
         }
     }
 }
