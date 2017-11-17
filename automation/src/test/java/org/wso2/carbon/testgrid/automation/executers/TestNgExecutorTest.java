5eed343e7675d509545f228c3a940fb1c9fd954c
asma <azinneera@gmail.com>
2017-11-17 17:26:47 +0530

    Improve unit test coverage in automation module

---
+++ automation/src/test/java/org/wso2/carbon/testgrid/automation/executers/TestNgExecutorTest.java
@@ -0,0 +1,22 @@
+package org.wso2.carbon.testgrid.automation.executers;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class TestNgExecutorTest {
+    @BeforeMethod
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void testExecute() throws Exception {
+
+    }
+
+    @Test
+    public void testInit() throws Exception {
+    }
+
+}
