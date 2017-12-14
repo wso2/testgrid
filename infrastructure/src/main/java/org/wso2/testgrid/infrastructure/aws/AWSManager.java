2c15d0c059c1f436732b85bf318540d6d7dd7cc2
asma <azinneera@gmail.com>
2017-12-14 20:15:41 +0530

    Fix unused import checkstyle error

--- infrastructure/src/main/java/org/wso2/testgrid/infrastructure/aws/AWSManager.java
+++ infrastructure/src/main/java/org/wso2/testgrid/infrastructure/aws/AWSManager.java
@@ -46,7 +46,6 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Properties;
 
