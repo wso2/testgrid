1da6fba9a2dbbab46b564a7813e72bee096917c3
Pasindu <pasinduonline@gmail.com>
2018-03-07 14:52:56 +0530

    Remove registering service-worker.

--- web/src/main/react-dashboard/src/index.js
+++ web/src/main/react-dashboard/src/index.js
@@ -20,11 +20,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter } from 'react-router-dom'
 /*This is the main component of the dash board*/ 
 ReactDOM.render(
     <BrowserRouter >
         <App />
     </BrowserRouter>, document.getElementById('root'));
-registerServiceWorker();
