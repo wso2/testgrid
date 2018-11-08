9440a6987805ca8e1a38f94b1b8541137d99924e
tharindu <tharindu@WSO2s-MacBook-Pro.local>
2018-11-08 09:16:09 +0530

    Add new token for backend API request authentication

--- web/src/main/java/org/wso2/testgrid/web/sso/SSOSessionCheckFilter.java
+++ web/src/main/java/org/wso2/testgrid/web/sso/SSOSessionCheckFilter.java
@@ -68,6 +68,14 @@ public class SSOSessionCheckFilter implements Filter {
                 String path = ((HttpServletRequest) servletRequest).getRequestURI();
                 if (isSecuredAPI(path)) {
                     Boolean isSessionValid = ((HttpServletRequest) servletRequest).isRequestedSessionIdValid();
+                    if (!StringUtil.isStringNullOrEmpty(
+                            ((HttpServletRequest) servletRequest).getHeader("Authorization"))) {
+                        if (((HttpServletRequest) servletRequest).getHeader("Authorization").equals
+                                (ConfigurationContext.getProperty(ConfigurationProperties.API_TOKEN))) {
+                            isSessionValid = true;
+                        }
+                    }
+
                     if (!isSessionValid) {
                         HttpServletResponse httpResponse = (HttpServletResponse) servletResponse;
                         //If the request is for a backend API, Status Code 401 is sent.
