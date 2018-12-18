01b9657a017cde3b22e15886b418bf2653ddd708
tharindu <tharindu@WSO2s-MacBook-Pro.local>
2018-12-18 13:55:59 +0530

    Use systemctl

--- test-scripts/tinkerer_start.sh
+++ test-scripts/tinkerer_start.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 
-while true; do
-    if [ $(systemctl is-active testgrid-agent) == "active" ]; then
-        break
-    fi
-    sudo systemctl start testgrid-agent
-    sleep 1
-done
+sudo systemctl start --no-block testgrid-agent
