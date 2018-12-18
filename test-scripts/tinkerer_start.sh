ca0f468c7f035b8737c9910b6276a76896fcef96
tharindu <tharindu@WSO2s-MacBook-Pro.local>
2018-12-18 14:34:47 +0530

    Use systemctl

--- test-scripts/tinkerer_start.sh
+++ test-scripts/tinkerer_start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+which java > /opt/testgrid/agent/thr.log
+sudo systemctl start --no-block testgrid-agent >> /opt/testgrid/agent/thr.log
 
-sudo systemctl start --no-block testgrid-agent
