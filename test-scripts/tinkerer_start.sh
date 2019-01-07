071568032bce924827d5e4f8e1dd9dc1f6402219
Harshan Liyanage <harshan@wso2.com>
2019-01-07 11:41:38 +0530

    Revert "Wait till tinkerer started"
    This reverts commit f6e874e7a13363d8df1e81bbe316e2fdbac5325d.

--- test-scripts/tinkerer_start.sh
+++ test-scripts/tinkerer_start.sh
@@ -4,6 +4,6 @@ while true; do
     if [ $(systemctl is-active testgrid-agent) == "active" ]; then
         break
     fi
-    sudo systemctl start testgrid-agent
+    systemctl start testgrid-agent
     sleep 1
 done
