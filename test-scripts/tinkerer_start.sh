074c026f60c076a054fb4d5d7b5f3ea9d62698df
Harshan Liyanage <harshan@wso2.com>
2019-01-07 11:41:38 +0530

    Revert "Use systemctl"
    This reverts commit ca0f468c7f035b8737c9910b6276a76896fcef96.

--- test-scripts/tinkerer_start.sh
+++ test-scripts/tinkerer_start.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-which java > /opt/testgrid/agent/thr.log
-sudo systemctl start --no-block testgrid-agent >> /opt/testgrid/agent/thr.log
 
+sudo systemctl start --no-block testgrid-agent
