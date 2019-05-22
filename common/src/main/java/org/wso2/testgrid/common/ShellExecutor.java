909e26d414c3567675cf094f86a666617fe1a024
chathurangie <edussuriyac@gmail.com>
2019-05-22 17:12:09 +0530

    modify-config-properties

--- common/src/main/java/org/wso2/testgrid/common/ShellExecutor.java
+++ common/src/main/java/org/wso2/testgrid/common/ShellExecutor.java
@@ -95,10 +95,6 @@ public class ShellExecutor {
      */
     public int executeCommand(String command) throws CommandExecutionException {
 
-<<<<<<< HEAD
-=======
-
->>>>>>> ced188041a2c6e9bd5aba6857ff4206319e2a7d3
         logger.info("Running shell command : " + command + ", from working directory : " + workingDirectory);
 
         ProcessBuilder processBuilder = new ProcessBuilder("/bin/bash", "-c", command);
