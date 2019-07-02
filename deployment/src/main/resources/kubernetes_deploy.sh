1c2a5507c7a6cffc83d9895ae72ddb8c273ccd1c
chathurangie <edussuriyac@gmail.com>
2019-07-02 15:44:19 +0530

    modify from loadbalancer to ingress and add k8s_output.sh seperately for abstraction

--- deployment/src/main/resources/kubernetes_deploy.sh
+++ deployment/src/main/resources/kubernetes_deploy.sh
@@ -18,11 +18,14 @@
 
 set -o xtrace
 echo "deploy file is found"
+dryRun=False
 
 OUTPUT_DIR=$4
 INPUT_DIR=$2
 source $INPUT_DIR/infrastructure.properties
+source $OUTPUT_DIR/deployment.properties
 
+cat $OUTPUT_DIR/deployment.properties
 #definitions
 
 YAMLS=$yamls
@@ -32,9 +35,13 @@ no_yamls=${#yamls[@]}
 dep=($deployments)
 dep_num=${#dep[@]}
 
-function create_resources() {
+function create_k8s_resources() {
 
-
+    if [ -z $YAMLS ]
+    then
+      echo "the yaml file is not created or the yaml file is not available"
+      exit 1
+    fi
     #create the deployments
 
     if [ -z $deployments ]
@@ -43,32 +50,97 @@ function create_resources() {
       exit 1
     fi
 
+
     if [ -z $yamlFilesLocation ]; then
       echo "the yaml files location is not given"
       exit 1
+    fi
+
+    if [ -z ${loadBalancerHostName} ]; then
+        echo WARN: loadBalancerHostName not found in deployment.properties. Generating a random name under \
+        *.gke.wso2testgrid.com CN
+        loadBalancerHostName=wso2am-$(($RANDOM % 10000)).gke.wso2testgrid.com # randomized hostname
     else
-      i=0;
-      for ((i=0; i<$no_yamls; i++))
-      do
-        kubectl create -f $yamlFilesLocation/${yamls[$i]}
-      done
+        echo DEBUG: loadBalanceHostName: ${loadBalancerHostName}
+    fi
+
+    if [[ ${dryRun^^} != TRUE ]]; then
+        i=0;
+        for ((i=0; i<$no_yamls; i++))
+        do
+          kubectl create -f $yamlFilesLocation/${yamls[$i]}
+        done
     fi
 
     readiness_deployments
     sleep 10
 
+# TODO: install ingress-nginx controller if not found.
+
+# Create a ingress for the services we want to expose to public internet.
+tlskeySecret=testgrid-certs
+ingressName=tg-ingress
+kubectl create secret tls ${tlskeySecret} \
+    --cert deploymentRepository/keys/testgrid-certs-v2.crt  \
+    --key deploymentRepository/keys/testgrid-certs-v2.key -n $namespace
+
+    cat > ${ingressName}.yaml << EOF
+apiVersion: extensions/v1beta1
+kind: Ingress
+metadata:
+  name: ${ingressName}
+  namespace: ${namespace}
+  annotations:
+    kubernetes.io/ingress.class: "nginx"
+    nginx.ingress.kubernetes.io/backend-protocol: "HTTPS"
+spec:
+  tls:
+  - hosts:
+    - mgt-${loadBalancerHostName}
+    - gw-${loadBalancerHostName}
+    - ${loadBalancerHostName}
+    secretName: ${tlskeySecret}
+  rules:
+EOF
     i=0;
     for ((i=0; i<$dep_num; i++))
     do
-      kubectl expose deployment ${dep[$i]} --name=${dep[$i]}  --type=LoadBalancer -n $namespace
+      echo
+      kubectl expose deployment ${dep[$i]} --name=${dep[$i]} -n $namespace
+#      kubectl expose deployment ${dep[$i]} --name=${dep[$i]}  --type=LoadBalancer -n $namespace
+      cat >> ${ingressName}.yaml << EOF
+  - host: mgt-${loadBalancerHostName}
+    http:
+      paths:
+      - backend:
+          serviceName: ${dep[$i]}
+          servicePort: 9443 # TODO: FIX THIS - this also need to come from the testgrid.yaml.
+  - host: gw-${loadBalancerHostName}
+    http:
+      paths:
+      - backend:
+          serviceName: ${dep[$i]}
+          servicePort: 8243 # TODO: FIX THIS - this also need to come from the testgrid.yaml.
+  - host: ${loadBalancerHostName}
+    http:
+      paths:
+      - backend:
+          serviceName: ${dep[$i]}
+          servicePort: 9443 # TODO: FIX THIS - this also need to come from the testgrid.yaml.
+EOF
     done
+    echo Final ingress yaml:
+    cat ${ingressName}.yaml
+    kubectl apply -f ${ingressName}.yaml -n $namespace
 
     readinesss_services
 
     echo "namespace=$namespace" >> $OUTPUT_DIR/deployment.properties
+    echo "loadBalancerHostName=$loadBalancerHostName" >> $OUTPUT_DIR/deployment.properties
 }
 
 function readiness_deployments(){
+    start=`date +%s`
     i=0;
     # todo add a terminal condition/timeout.
     for ((i=0; i<$dep_num; i++)) ; do
@@ -81,26 +153,98 @@ function readiness_deployments(){
         fi
       done
     done
+
+    end=`date +%s`
+    runtime=$((end-start))
+    echo "Deployment \"${dep}\" got ready in ${runtime} seconds."
+    echo
 }
 
 function readinesss_services(){
+    start=`date +%s`
     i=0;
     for ((i=0; i<$dep_num; i++)); do
       external_ip=""
       echo "Getting the ingress IP address for ingress: ${ingressName}"
       while [ -z $external_ip ]; do
         echo "Waiting for end point..."
-        external_ip=$(kubectl get service ${dep[$i]} --template="{{range .status.loadBalancer.ingress}}{{.ip}}{{end}}" --namespace ${namespace})
-        #external_ip=$(kubectl get ingress ${ingressName} --template="{{range .status.loadBalancer.ingress}}{{.ip}}{{end}}" --namespace ${namespace})
+#        external_ip=$(kubectl get service ${dep[$i]} --template="{{range .status.loadBalancer.ingress}}{{.ip}}{{end}}" --namespace ${namespace})
+        external_ip=$(kubectl get ingress ${ingressName} --template="{{range .status.loadBalancer.ingress}}{{.ip}}{{end}}" --namespace ${namespace})
         [ -z "$external_ip" ] && sleep 10
       done
-       echo "PublisherUrl=https://$external_ip:9443/publisher" >> $OUTPUT_DIR/deployment.properties
-      echo "StoreUrl=https://$external_ip:9443/store" >> $OUTPUT_DIR/deployment.properties
-      echo "AdminUrl=https://$external_ip:9443/admin" >> $OUTPUT_DIR/deployment.properties
-      echo "CarbonServerUrl=https://$external_ip:9443/services/" >> $OUTPUT_DIR/deployment.properties
-      echo "CarbonServerUrl=https://$external_ip:9443/services/" >> $OUTPUT_DIR/deployment.properties
-      echo "GatewayHttpsUrl=https://$external_ip:8243" >> $OUTPUT_DIR/deployment.properties
-      done
+    echo "loadBalancerHostName=${loadBalancerHostName}" >> $OUTPUT_DIR/deployment.properties
+    echo "loadBalancerIP=${external_ip}" >> $OUTPUT_DIR/deployment.properties
+
+    done
+
+    end=`date +%s`
+    runtime=$((end-start))
+    echo "Kubernetes Ingress service '${ingressName}' got ready in ${runtime} seconds."
+
 }
 
-create_resources
+function add_route53_entry() {
+    env=${TESTGRID_ENVIRONMENT} || 'dev'
+    if [[ "${env}" != "dev" ]] && [[ "${env}" != 'prod' ]]; then
+        echo "Not configuring route53 DNS entries since the environment is not dev/prod. You need to manually add
+        '${external_ip} ${loadBalancerHostName}' into your /etc/hosts."
+        return;
+    fi
+
+    command -v aws >/dev/null 2>&1 || { echo >&2 "I optionally require aws but it's not installed. "; return; }
+    echo "Adding route53 entry to access Kubernetes ingress from the AWS ec2 instances."
+    echo "IP/Host mapping: ${external_ip} ${loadBalancerHostName}"
+    echo
+    testgrid_hosted_zone_id=$(aws route53 list-hosted-zones --query "HostedZones[?Name=='wso2testgrid.com.'].Id" --output text)
+
+    if [[ "$?" -ne 0 ]]; then
+        echo
+        echo "WARN: Failed to list hosted zones. Check whether you have enough AWS permissions. Route53 entry creation aborted."
+        echo
+        return;
+    fi
+
+    cat > route53-change-resource-record-sets.json << EOF
+{
+  "Comment": "testgrid job change batch req for mapping - ${external_ip} ${loadBalancerHostName}",
+  "Changes": [
+    {
+      "Action": "CREATE",
+      "ResourceRecordSet": {
+        "Name": "mgt-${loadBalancerHostName}", "Type": "A", "TTL": 60,
+        "ResourceRecords": [ { "Value": "${external_ip}" } ]
+      }
+    },
+    {
+      "Action": "CREATE",
+      "ResourceRecordSet": {
+        "Name": "gw-${loadBalancerHostName}", "Type": "A", "TTL": 60,
+        "ResourceRecords": [ { "Value": "${external_ip}" } ]
+      }
+    },
+    {
+      "Action": "CREATE",
+      "ResourceRecordSet": {
+        "Name": "${loadBalancerHostName}", "Type": "A", "TTL": 60,
+        "ResourceRecords": [ { "Value": "${external_ip}" } ]
+      }
+    }
+  ]
+}
+EOF
+cat route53-change-resource-record-sets.json
+
+change_id=$(aws route53 change-resource-record-sets --hosted-zone-id ${testgrid_hosted_zone_id} \
+    --change-batch file://route53-change-resource-record-sets.json \
+    --query "ChangeInfo.Id" --output text)
+aws route53 wait resource-record-sets-changed --id ${change_id}
+
+echo "AWS Route53 DNS server configured to access the ingress IP  ${external_ip} via hostname ${loadBalancerHostName}"
+echo
+}
+
+#DEBUG parameters: TODO: remove
+TESTGRID_ENVIRONMENT=dev
+
+create_k8s_resources
+add_route53_entry
