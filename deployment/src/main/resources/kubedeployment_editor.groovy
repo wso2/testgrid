aa44aea82f903042a7356aad2d5bfc1d09c66f14
Ranika <ranikamadurawe@gmail.com>
2019-08-29 15:25:39 +0530

    fix code review changes

--- deployment/src/main/resources/kubedeployment_editor.groovy
+++ deployment/src/main/resources/kubedeployment_editor.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ * Copyright (c) 2019, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
  *
  * WSO2 Inc. licenses this file to you under the Apache License,
  * Version 2.0 (the "License"); you may not use this file except
@@ -15,9 +15,6 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-
-
-
 
 @Grapes(
         @Grab(group='org.yaml', module='snakeyaml', version='1.24')
@@ -48,9 +45,41 @@ def AddNewItem(Map variable, String propertyName , Object new_Value){
 }
 
 /**
- * This method reads the config.properties file
+ * Formats the user provided inputs to a form that can be utilized by the sidecar container and log extraction
+ * container
  *
+ * @param loglocations JSONArray containing the log locations
+ * @param i index of the required JSONobject in the array
+ * @return
  */
+
+def Formattedfilepaths(JSONArray loglocations, Integer i){
+
+    String containerFilepath
+    String sidecarFilepath
+    if( loglocations.getJSONObject(i).get("path").toString().startsWith('/')
+            && loglocations.getJSONObject(i).get("path").toString().endsWith('/') ){
+        containerFilepath = loglocations.getJSONObject(i).get("path")
+        sidecarFilepath = ("/opt/tests/"+loglocations.getJSONObject(i).get("deploymentname")+"/"+
+                loglocations.getJSONObject(i).get("containername")+loglocations.getJSONObject(i).get("path"))
+    }else if( loglocations.getJSONObject(i).get("path").toString().startsWith('/')
+            && !loglocations.getJSONObject(i).get("path").toString().endsWith('/') ){
+        containerFilepath = loglocations.getJSONObject(i).get("path")+"/"
+        sidecarFilepath = ("/opt/tests/"+loglocations.getJSONObject(i).get("deploymentname")+"/"+
+                loglocations.getJSONObject(i).get("containername")+loglocations.getJSONObject(i).get("path")+"/")
+    }else if( !loglocations.getJSONObject(i).get("path").toString().startsWith('/')
+            && loglocations.getJSONObject(i).get("path").toString().endsWith('/') ){
+        containerFilepath = "/"+loglocations.getJSONObject(i).get("path")
+        sidecarFilepath = ("/opt/tests/"+loglocations.getJSONObject(i).get("deploymentname")+"/"+
+                loglocations.getJSONObject(i).get("containername")+"/"+loglocations.getJSONObject(i).get("path"))
+    }else{
+        containerFilepath = "/"+loglocations.getJSONObject(i).get("path")+"/"
+        sidecarFilepath = ("/opt/tests/"+loglocations.getJSONObject(i).get("deploymentname")+"/"+
+                loglocations.getJSONObject(i).get("containername")+"/"+loglocations.getJSONObject(i).get("path")+"/")
+    }
+    return [ containerFilepath, sidecarFilepath ]
+
+}
 
 
 /**
@@ -60,8 +89,7 @@ def AddNewItem(Map variable, String propertyName , Object new_Value){
  * @param pathToDeployment - file path to the input Yaml file
  *
  */
-def EditDeployments(String outputYaml,String jsonFilePath, String pathToDeployment){
-
+def EditK8SDeployments(String outputYaml,String jsonFilePath, String pathToDeployment){
     try{
         // Read json file
         InputStream is = new FileInputStream(jsonFilePath.toString())
@@ -108,7 +136,8 @@ def EditDeployments(String outputYaml,String jsonFilePath, String pathToDeployme
                             for (; i < loglocations.length(); i++) {
                                 JSONObject temp = loglocations.getJSONObject(i)
                                 // When found break the loop
-                                if(temp.getString("deploymentname").equals( depmeta.get("name") ) && temp.getString("containername").equals(container.get("name"))) {
+                                if(temp.getString("deploymentname").equals( depmeta.get("name") )
+                                        && temp.getString("containername").equals(container.get("name"))) {
                                     hasSidecarbeenAdded = true
                                     matchfound = true
                                     break
@@ -116,28 +145,23 @@ def EditDeployments(String outputYaml,String jsonFilePath, String pathToDeployme
                             }
                             // If a match is found enter the updated container into the list
                             if (matchfound){
-                                String containerFilepath
-                                String sidecarFilepath
-                                if( loglocations.getJSONObject(i).get("path").toString().startsWith('/') && loglocations.getJSONObject(i).get("path").toString().endsWith('/') ){
-                                    containerFilepath = loglocations.getJSONObject(i).get("path")
-                                    sidecarFilepath = "/opt/tests"+loglocations.getJSONObject(i).get("deploymentname")+"/"+loglocations.getJSONObject(i).get("containername")+loglocations.getJSONObject(i).get("path")
-                                }else if( loglocations.getJSONObject(i).get("path").toString().startsWith('/') && !loglocations.getJSONObject(i).get("path").toString().endsWith('/') ){
-                                    containerFilepath = loglocations.getJSONObject(i).get("path")+"/"
-                                    sidecarFilepath = "/opt/tests"+loglocations.getJSONObject(i).get("deploymentname")+"/"+loglocations.getJSONObject(i).get("containername")+loglocations.getJSONObject(i).get("path")+"/"
-                                }else if( !loglocations.getJSONObject(i).get("path").toString().startsWith('/') && loglocations.getJSONObject(i).get("path").toString().endsWith('/') ){
-                                    containerFilepath = "/"+loglocations.getJSONObject(i).get("path")
-                                    sidecarFilepath = "/opt/tests/"+loglocations.getJSONObject(i).get("deploymentname")+"/"+loglocations.getJSONObject(i).get("containername")+loglocations.getJSONObject(i).get("path")
-                                }else{
-                                    containerFilepath = "/"+loglocations.getJSONObject(i).get("path")+"/"
-                                    sidecarFilepath = "/opt/tests/"+loglocations.getJSONObject(i).get("deploymentname")+"/"+loglocations.getJSONObject(i).get("containername")+loglocations.getJSONObject(i).get("path")+"/"
-                                }
+
+                                List formattedPaths = Formattedfilepaths(loglocations,i)
+                                String containerFilepath = formattedPaths[0]
+                                String sidecarFilepath = formattedPaths[1]
 
                                 // New volume mount for the container
                                 // new volume mount for the sidecar container
                                 // Echo the path into a file in the sidecar container so it knows that it is a log path
-                                Map new_VolumeMount = ["name":"logfilesmount"+logcontainers, "mountPath": containerFilepath ]
-                                SidecarVolMounts.add(["name":"logfilesmount"+logcontainers, "mountPath": sidecarFilepath])
-                                CommandString = CommandString + "echo executearchive "+ sidecarFilepath +" logfilesmount"+logcontainers+ " " + loglocations.getJSONObject(i).get("deploymentname") + " " + loglocations.getJSONObject(i).get("containername")+" >> log_archiver.sh &&"
+                                Map new_VolumeMount =
+                                        ["name":"logfilesmount"+logcontainers, "mountPath": containerFilepath]
+                                SidecarVolMounts
+                                        .add(["name":"logfilesmount"+logcontainers, "mountPath": sidecarFilepath])
+                                CommandString = ( CommandString + "echo executearchive "+ sidecarFilepath +
+                                        " logfilesmount"+ logcontainers+ " " +
+                                        loglocations.getJSONObject(i).get("deploymentname") + " " +
+                                        loglocations.getJSONObject(i).get("containername") +" >> log_archiver.sh &&"
+                                )
 
                                 newcontainerlist.add(AddNewItem(container,"volumeMounts",new_VolumeMount))
                                 logcontainers++
@@ -153,35 +177,50 @@ def EditDeployments(String outputYaml,String jsonFilePath, String pathToDeployme
                     if(hasSidecarbeenAdded){
                         // Add the updated container list as the deployment yamls container list
 
-
                         /*
                         Change to persistent disk claim if using persistent disk
                          */
                         Map emptyMap = [:]
                         for (int j = 0; j<logcontainers;j++){
                             Map new_Volume = ["name": "logfilesmount"+j , "emptyDir": emptyMap ]
-                            group.get("spec").get("template").put("spec",AddNewItem(group.get("spec").get("template").get("spec"),"volumes",new_Volume))
+                            group.get("spec").get("template").put("spec",AddNewItem(
+                                    group.get("spec").get("template").get("spec"),"volumes",new_Volume))
                         }
 
+                        JSONObject currentscriptParams = json.getJSONObject("currentscript")
                         /*
-                       Remove entirely if using persistent disk
+                         Remove entirely if using persistent disk
                         */
                         CommandString = CommandString + " echo transfer >> log_archiver.sh && "
-                        Map new_Container = [ "name": "logfile-sidecar" , "image":"ranikamadurawe/mytag", "volumeMounts":SidecarVolMounts,
-                                              "env": [ ["name": "nodename" , "valueFrom" : ["fieldRef" : ["fieldPath" : "spec.nodeName"]] ],
-                                                       ["name": "podname" , "valueFrom" : ["fieldRef" : ["fieldPath" : "metadata.name"]] ],
-                                                       ["name": "podnamespace" , "valueFrom" : ["fieldRef" : ["fieldPath" : "metadata.namespace"]] ],
-                                                       ["name": "podip" , "valueFrom" : ["fieldRef" : ["fieldPath" : "status.podIP"]] ],
-                                                       ["name": "wsEndpoint" , "value": json.getJSONObject("currentscript").getString("DEPLOYMENT_TINKERER_EP") ],
-                                                       /* --NEED TO CHANGE -- */                ["name": "region" , "value": "US"  ],
-                                                       ["name": "provider" , "value": "K8S" ],
-                                                       ["name": "testPlanId" , "value": json.getJSONObject("currentscript").getString("tpID")  ],
-                                                       ["name": "userName" , "value": json.getJSONObject("currentscript").getString("DEPLOYMENT_TINKERER_USERNAME") ],
-                                                       ["name": "password" , "value": json.getJSONObject("currentscript").getString("DEPLOYMENT_TINKERER_PASSWORD") ],
-                                              ],
-                                              "command": ["/bin/bash", "-c", CommandString+"./kubernetes_startup.sh && tail -f /dev/null" ]
-                        ]
-                        group.get("spec").get("template").put("spec",AddNewItem(group.get("spec").get("template").get("spec"),"containers",new_Container))
+                        Map new_Container =
+                                [ "name": "logfile-sidecar" ,
+                                  "image":"ranikamadurawe/mytag",
+                                  "volumeMounts":SidecarVolMounts,
+                                  "env": [
+                                          ["name": "nodename" ,
+                                           "valueFrom" : ["fieldRef" : ["fieldPath" : "spec.nodeName"]]],
+                                          ["name": "podname" ,
+                                           "valueFrom" : ["fieldRef" : ["fieldPath" : "metadata.name"]]],
+                                          ["name": "podnamespace" ,
+                                           "valueFrom" : ["fieldRef" : ["fieldPath" : "metadata.namespace"]]],
+                                          ["name": "podip" ,
+                                           "valueFrom" : ["fieldRef" : ["fieldPath" : "status.podIP"]]],
+                                          ["name": "wsEndpoint" ,
+                                           "value": currentscriptParams.getString("DEPLOYMENT_TINKERER_EP")],
+                                          ["name": "region" , "value": "US"  ],
+                                          ["name": "provider" , "value": "K8S" ],
+                                          ["name": "testPlanId" ,
+                                           "value": currentscriptParams.getString("tpID")  ],
+                                          ["name": "userName" ,
+                                           "value": currentscriptParams.getString("DEPLOYMENT_TINKERER_USERNAME")],
+                                          ["name": "password" ,
+                                           "value": currentscriptParams.getString("DEPLOYMENT_TINKERER_PASSWORD")],
+                                  ],
+                                  "command": ["/bin/bash", "-c",
+                                              CommandString+"./kubernetes_startup.sh && tail -f /dev/null" ]
+                                ]
+                        group.get("spec").get("template").put("spec",AddNewItem(
+                                group.get("spec").get("template").get("spec"),"containers",new_Container))
                     }
 
                 }
@@ -194,13 +233,14 @@ def EditDeployments(String outputYaml,String jsonFilePath, String pathToDeployme
         }
 
         fileWriter.close()
-        new File("tpid.properties").delete()
+
     }catch(RuntimeException e){
         println(e)
     }
 }
 
-/*
-Args must be provided as --outputyaml_name --path_to_testLogs --path_to_Deployment.yaml testgrid_home
+/**
+ * Args must be provided in following order
+ * outputyaml_name   path_to_testLogs   path_to_Deployment.yaml
  */
-EditDeployments(args[0],args[1],args[2])
+EditK8SDeployments(args[0],args[1],args[2])
