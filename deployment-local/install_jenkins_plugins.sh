722d6f238c9502a4d599b95c625b90fc81809c7e
udhanMti <udhanisuranga@gmail.com>
2019-09-18 17:25:18 +0530

    added a missing jenkins plugin

--- deployment-local/install_jenkins_plugins.sh
+++ deployment-local/install_jenkins_plugins.sh
@@ -8,7 +8,7 @@ do
  sleep 5
 done
 
-if [ $(java -jar tomcat/apache-tomcat-8.5.43/webapps/admin/WEB-INF/jenkins-cli.jar -s $host groovy = < pluginsEnumerator.groovy) -eq 152 ]
+if [ $(java -jar tomcat/apache-tomcat-8.5.43/webapps/admin/WEB-INF/jenkins-cli.jar -s $host groovy = < pluginsEnumerator.groovy) -eq 153 ]
 then
   exit 0
 fi
@@ -73,8 +73,9 @@ curl -X POST -d '<jenkins><install plugin="view-job-filters@latest" /></jenkins>
 curl -X POST -d '<jenkins><install plugin="ws-cleanup@latest" /></jenkins>' --header 'Content-Type: text/xml' $host$url
 curl -X POST -d '<jenkins><install plugin="mission-control-view@latest" /></jenkins>' --header 'Content-Type: text/xml' $host$url
 curl -X POST -d '<jenkins><install plugin="configuration-as-code@latest" /></jenkins>' --header 'Content-Type: text/xml' $host$url
+curl -X POST -d '<jenkins><install plugin="pipeline-stage-view@latest" /></jenkins>' --header 'Content-Type: text/xml' $host$url
 
-while [ $(java -jar tomcat/apache-tomcat-8.5.43/webapps/admin/WEB-INF/jenkins-cli.jar -s $host groovy = < pluginsEnumerator.groovy) -lt 152 ]
+while [ $(java -jar tomcat/apache-tomcat-8.5.43/webapps/admin/WEB-INF/jenkins-cli.jar -s $host groovy = < pluginsEnumerator.groovy) -lt 153 ]
 do
    echo "plugins still installing.... "$(java -jar tomcat/apache-tomcat-8.5.43/webapps/admin/WEB-INF/jenkins-cli.jar -s $host groovy = < pluginsEnumerator.groovy)" installed"
    sleep 3
