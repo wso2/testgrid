ebd2cf44a638f3d1fe427aeed01be6b11cd331f2
udhanMti <udhanisuranga@gmail.com>
2019-09-23 10:20:43 +0530

    changed some file names

--- deployment-local/add_config_provider_files.sh
+++ deployment-local/add_config_provider_files.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
 host=http://localhost:8080/admin
-curl -X POST $host/configuration-as-code/replace?_.newSource=configuration_as_code.yaml&json={"newSource":"configuration_as_code.yaml"}&replace=Apply%20new%20configuration
+
+while [ $(curl -s -w "%{http_code}" $host/cli -o /dev/null) -eq 503 ]
+do
+ sleep 5
+done
+
+curl -X POST $host/configuration-as-code/replace?_.newSource=configuration_as_code.yaml&json={"newSource":"configuration.yaml"}&replace=Apply%20new%20configuration
