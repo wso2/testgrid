c5ee2b90626cbfeb61b37361a87d156a4ee9404a
NishikaDeSilva <desilvanishika73@gmail.com>
2020-06-24 19:07:28 +0530

    Update script content

--- jobs/intg-test-resources/scripts/test.sh
+++ jobs/intg-test-resources/scripts/test.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+WORKING_DIR=$(pwd)
+
 PRODUCT_REPOSITORY=$1
 PRODUCT_REPOSITORY_BRANCH=$2
-PRODUCT_REPOSITORY_NAME=$(echo $PRODUCT_REPOSITORY | rev | cut -d'/' -f1 | rev cut -d'.' -f1)
+PRODUCT_REPOSITORY_NAME=$(echo $PRODUCT_REPOSITORY | rev | cut -d'/' -f1 | rev | cut -d'.' -f1)
 LOCAL_PRODUCT_PACK_LOCATION="/mnt/$(echo $PRODUCT_REPOSITORY_NAME | cut -d'-' -f2)"
-PRODUCT_REPOSITORY_PACK_DIR="modules/distribution/product/target"
-INT_TEST_DIR="modules/integration"
+
+PRODUCT_REPOSITORY_PACK_DIR="$WORKING_DIR/$PRODUCT_REPOSITORY_NAME/modules/distribution/product/target"
+INT_TEST_MODULE_DIR="$WORKING_DIR/$PRODUCT_REPOSITORY_NAME/modules/integration"
+API_IMPORT_EXPORT_MODULE_DIR="$WORKING_DIR/$PRODUCT_REPOSITORY_NAME/modules/api-import-export"
 NEXUS_SCRIPT_NAME="uat-nexus-settings.xml"
 
 PRODUCT_NAME=$3
@@ -25,13 +29,14 @@ echo "Test script running"
 log_info "Clone Product repository"
 git clone https://$GIT_USER:$GIT_PASS@$PRODUCT_REPOSITORY --branch $PRODUCT_REPOSITORY_BRANCH
 
-mkdir -p $PRODUCT_REPOSITORY_NAME/$PRODUCT_REPOSITORY_PACK_DIR
+mkdir -p $PRODUCT_REPOSITORY_PACK_DIR
 
 log_info "Copying product pack to Repository"
-zip -qr $PRODUCT_NAME-$PRODUCT_VERSION.zip $LOCAL_PRODUCT_PACK_LOCATION/$PRODUCT_NAME-$PRODUCT_VERSION
-mv $PRODUCT_NAME-$PRODUCT_VERSION.zip $PRODUCT_REPOSITORY_NAME/$PRODUCT_REPOSITORY_PACK_DIR/.
-mv $NEXUS_SCRIPT_NAME $PRODUCT_REPOSITORY_NAME/$INT_TEST_DIR/.
+cd $LOCAL_PRODUCT_PACK_LOCATION && zip -qr $PRODUCT_NAME-$PRODUCT_VERSION.zip $PRODUCT_NAME-$PRODUCT_VERSION
+mv $PRODUCT_NAME-$PRODUCT_VERSION.zip $PRODUCT_REPOSITORY_PACK_DIR/.
+mv $WORKING_DIR/$NEXUS_SCRIPT_NAME $INT_TEST_MODULE_DIR/.
 
 source /etc/environment
 
-cd $PRODUCT_REPOSITORY_NAME/$INT_TEST_DIR  && mvn clean install -s $NEXUS_SCRIPT_NAME -fae -B -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn
+cd $API_IMPORT_EXPORT_MODULE_DIR && mvn clean install -fae -B -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn
+cd $INT_TEST_MODULE_DIR  && mvn clean install -s $NEXUS_SCRIPT_NAME -fae -B -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn
