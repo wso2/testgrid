38adbcf50f6fc5f24cb598e829308333365d9d39
NishikaDeSilva <desilvanishika73@gmail.com>
2020-06-24 19:06:40 +0530

    Add EIP attachment

--- jobs/intg-test-resources/scripts/copy_script.sh
+++ jobs/intg-test-resources/scripts/copy_script.sh
@@ -7,10 +7,18 @@ INPUTS_DIR=$2
 # SCRIPT_LOCATION=""
 SCRIPT_NAME="test.sh"
 PROP_FILE="${INPUTS_DIR}/deployment.properties"
-PRODUCT_GIT_URL=$(grep -w "PRODUCT_GIT_URL" ${PROP_FILE} | cut -d'=' -f2)
+PRODUCT_GIT_URL=$(grep -w "PRODUCT_GIT_URL" ${PROP_FILE} | cut -d'=' -f2 | cut -d'/' -f3-)
 PRODUCT_GIT_BRANCH=$(grep -w "PRODUCT_GIT_BRANCH" ${PROP_FILE} | cut -d'=' -f2)
 keyFileLocation=$(grep -w "keyFileLocation" ${PROP_FILE} | cut -d'=' -f2)
 WSO2InstanceName=$(grep -w "WSO2InstanceName" ${PROP_FILE} | cut -d'=' -f2 | cut -d"/" -f3)
+PRODUCT_NAME=$(grep -w "WSO2_PRODUCT" ${PROP_FILE}| cut -d'=' -f2 | cut -d'-' -f1)
+PRODUCRT_VERSION=$(grep -w "WSO2_PRODUCT" ${PROP_FILE}| cut -d'=' -f2 | cut -d'-' -f2)
+
+NEXUS_SCRIPT_NAME="uat-nexus-settings.xml"
+NEXUS_SCRIPT_PATH='/testgrid/testgrid-home/jobs/wso2am-2.1.0-int-test'
+
+GIT_USER=$(grep -w "VIM_GIT_USERNAME" ${PROP_FILE} | cut -d'=' -f2)
+GIT_PASS=$(grep -w "VIM_GIT_TOKEN" ${PROP_FILE} | cut -d'=' -f2)
 
 function log_info(){
     echo "[INFO][$(date '+%Y-%m-%d %H:%M:%S')]: $1"
@@ -18,10 +26,15 @@ function log_info(){
 
 log_info "Copying ${SCRIPT_NAME} to remote ec2 instance"
 
-scp -i ${keyFileLocation} ${SCRIPT_NAME} ubuntu@${WSO2InstanceName}:/opt/testgrid/workspace/${SCRIPT_NAME}
-ssh -i ${keyFileLocation} ubuntu@${WSO2InstanceName} "cd /opt/testgrid/workspace && bash ${SCRIPT_NAME} ${PRODUCT_GIT_URL} ${PRODUCT_GIT_BRANCH}"
-ssh -i ${keyFileLocation} ubuntu@${WSO2InstanceName} "ls /opt/testgrid/workspace"
-ssh -i ${keyFileLocation} ubuntu@${WSO2InstanceName} "cd /opt/testgrid/workspace/product-apim && git branch"
+ls -la ${keyFileLocation}
+chmod 400 ${keyFileLocation}
+ls -la ${keyFileLocation}
+
+scp -o StrictHostKeyChecking=no -i ${keyFileLocation} ${SCRIPT_NAME} ubuntu@${WSO2InstanceName}:/opt/testgrid/workspace/${SCRIPT_NAME}
+scp -o StrictHostKeyChecking=no -i ${keyFileLocation} ${NEXUS_SCRIPT_PATH}/${NEXUS_SCRIPT_NAME} ubuntu@${WSO2InstanceName}:/opt/testgrid/workspace/${NEXUS_SCRIPT_NAME}
+ssh -o StrictHostKeyChecking=no -i ${keyFileLocation} ubuntu@${WSO2InstanceName} "cd /opt/testgrid/workspace && bash ${SCRIPT_NAME} ${PRODUCT_GIT_URL} ${PRODUCT_GIT_BRANCH} ${PRODUCT_NAME} ${PRODUCRT_VERSION} ${GIT_USER} ${GIT_PASS}"
+ssh -o StrictHostKeyChecking=no -i ${keyFileLocation} ubuntu@${WSO2InstanceName} "ls /opt/testgrid/workspace"
+# ssh -i ${keyFileLocation} ubuntu@${WSO2InstanceName} "cd /opt/testgrid/workspace/product-apim && git branch"
 
 sleep 3600
 
--- jobs/intg-test-resources/scripts/test.sh
+++ jobs/intg-test-resources/scripts/test.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-# LOCAL_PRODUCT_PACK_LOCATION=""
 PRODUCT_REPOSITORY=$1
 PRODUCT_REPOSITORY_BRANCH=$2
-# PRODUCT_REPOSITORY_PACK_DIR=""
+PRODUCT_REPOSITORY_NAME=$(echo $PRODUCT_REPOSITORY | rev | cut -d'/' -f1 | rev cut -d'.' -f1)
+LOCAL_PRODUCT_PACK_LOCATION="/mnt/$(echo $PRODUCT_REPOSITORY_NAME | cut -d'-' -f2)"
+PRODUCT_REPOSITORY_PACK_DIR="modules/distribution/product/target"
+INT_TEST_DIR="modules/integration"
+NEXUS_SCRIPT_NAME="uat-nexus-settings.xml"
 
-# PRODUCT_NAME=""
-# PRODUCT_VERSION=""
+PRODUCT_NAME=$3
+PRODUCT_VERSION=$4
+
+GIT_USER=$5
+GIT_PASS=$6
 
 set -o xtrace
 
@@ -17,7 +23,15 @@ function log_info(){
 echo "Test script running"
 
 log_info "Clone Product repository"
-git clone $PRODUCT_REPOSITORY --branch $PRODUCT_REPOSITORY_BRANCH
+git clone https://$GIT_USER:$GIT_PASS@$PRODUCT_REPOSITORY --branch $PRODUCT_REPOSITORY_BRANCH
+
+mkdir -p $PRODUCT_REPOSITORY_NAME/$PRODUCT_REPOSITORY_PACK_DIR
 
 log_info "Copying product pack to Repository"
-# cp $LOCAL_PRODUCT_PACK_LOCATION/$PRODUCT_NAME-$PRODUCT_VERSION.zip $PRODUCT_REPOSITORY_PACK_DIR/$PRODUCT_NAME-$PRODUCT_VERSION.zip
+zip -qr $PRODUCT_NAME-$PRODUCT_VERSION.zip $LOCAL_PRODUCT_PACK_LOCATION/$PRODUCT_NAME-$PRODUCT_VERSION
+mv $PRODUCT_NAME-$PRODUCT_VERSION.zip $PRODUCT_REPOSITORY_NAME/$PRODUCT_REPOSITORY_PACK_DIR/.
+mv $NEXUS_SCRIPT_NAME $PRODUCT_REPOSITORY_NAME/$INT_TEST_DIR/.
+
+source /etc/environment
+
+cd $PRODUCT_REPOSITORY_NAME/$INT_TEST_DIR  && mvn clean install -s $NEXUS_SCRIPT_NAME -fae -B -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn
