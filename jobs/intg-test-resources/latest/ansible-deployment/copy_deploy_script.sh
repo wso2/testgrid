4022b56cc3c8b4aa78c5bdc3fee7c7e2431e1426
NishikaDeSilva <desilvanishika73@gmail.com>
2020-07-02 10:29:32 +0530

    Add ansible deploy scripts

---
+++ jobs/intg-test-resources/latest/ansible-deployment/copy_deploy_script.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -o xtrace;
+
+INPUT_DIR=$2
+
+WORKING_DIR=$(pwd)
+TEST_PROP_FILE=$INPUT_DIR/testplan-props.properties
+INFRA_PROP_FILE=$INPUT_DIR/infrastructure.properties
+GIT_USER=$(grep -w "GIT_WUM_USERNAME" $TEST_P{ROP_FILE | cut -d'=' -f2)
+GIT_PASS=$(grep -w "GIT_WUM_PASSWORD" ${TEST_PROP_FILE} | cut -d'=' -f2)
+keyFileLocation=$(grep -w "keyFileLocation" ${TEST_PROP_FILE} | cut -d'=' -f2)
+
+OS=$(grep  -w "OS" ${INFRA_PROP_FILE} | cut -d'=' -f2)
+WSO2InstanceName=$(grep -w "WSO2InstanceName" ${INFRA_PROP_FILE} | cut -d'=' -f2 | cut -d"/" -f3)
+
+ANSIBLE_REPO=$(grep -w "ANSIBLE_GIT_URL" $TEST_PROP_FILE | cut -d'=' -f2 | cut -d'/' -f3-)
+ANSIBLE_BRANCH=$(grep -w "ANSIBLE_GIT_BRANCH" $TEST_PROP_FILE | cut -d'=' -f2)
+PRODUCT_NAME=$(grep -w "WSO2_PRODUCT" ${TEST_PROP_FILE}| cut -d'=' -f2 | cut -d'-' -f1)
+PRODUCT_VERSION=$(grep -w "WSO2_PRODUCT" ${TEST_PROP_FILE}| cut -d'=' -f2 | cut -d'-' -f2)
+
+LOCAL_DEPLOYMENT_SCRIPT=${WORKING_DIR}/deploy.sh
+REMOTE_DEPLOYMENT_SCRIPT=/opt/testgrid/workspace/deploy.sh
+
+sed -i "s/'&ANSIBLE_REPO_URL'/${ANSIBLE_REPO}/g" ${DEPLOYMENT_SCRIPT}
+sed -i "s/'&ANSIBLE_REPO_BRANCH'/${ANSIBLE_BRANCH}/g" ${DEPLOYMENT_SCRIPT}
+
+if [ $OS = "ubuntu" ]; then
+    instanceUser="ubuntu"
+fi
+
+scp -o StrictHostKeyChecking=no -i ${keyFileLocation} ${LOCAL_DEPLOYMENT_SCRIPT} ${instanceUser}@${WSO2InstanceName}:${REMOTE_DEPLOYMENT_SCRIPT}
+ssh -o StrictHostKeyChecking=no -i ${keyFileLocation} ${instanceUser}@${WSO2InstanceName} "sudo bash ${REMOTE_DEPLOYMENT_SCRIPT} ${PRODUCT_NAME} ${PRODUCT_VERSION} ${GIT_USER} ${GIT_PASS}"
---
+++ jobs/intg-test-resources/latest/ansible-deployment/deploy.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+WSO2_PRODUCT_NAME=$1
+WSO2_PRODUCT_VERSION=$2
+GITUSER=$3
+GITPASS=$4
+
+WORKING_DIR=$(pwd)
+
+CFN_PROP_FILE=/opt/testgrid/workspace/cfn-props.properties
+ANSIBLE_REPO_NAME=$(echo '&ANSIBLE_REPO_URL' | rev | cut -d'/' -f1 | rev)
+DB_PROV_SCRIPT_NAME="provision_db_${WSO2_PRODUCT_NAME}.sh"
+DB_PROV_SCRIPT_PATH="${WORKING_DIR}/${DB_PROV_SCRIPT_NAME}"
+
+# get aws properties
+DBEngine=$(grep -w "DB_TYPE" ${CFN_PROP_FILE} | cut -d'=' -f2)
+DBEngineVersion=$(grep -w "CF_DB_VERSION" ${CFN_PROP_FILE} | cut -d'=' -f2)
+JDK=$(grep -w "JDK_TYPE" ${CFN_PROP_FILE} | cut -d'=' -f2)
+DBPassword=$(grep -w "CF_DB_PASSWORD" ${CFN_PROP_FILE} | cut -d'=' -f2)
+DBUsername=$(grep -w "CF_DB_USERNAME" ${CFN_PROP_FILE} | cut -d'=' -f2)
+DBHost=$(grep -w "CF_DB_HOST" ${CFN_PROP_FILE} | cut -d'=' -f2)
+DBPort=$(grep -w "CF_DB_PORT" ${CFN_PROP_FILE} | cut -d'=' -f2)
+SID=$(grep -w "SID" ${CFN_PROP_FILE} | cut -d'=' -f2)
+
+function log_info(){
+    echo "[INFO][$(date '+%Y-%m-%d %H:%M:%S')]: $1"
+}
+
+function log_error(){
+    echo "[ERROR][$(date '+%Y-%m-%d %H:%M:%S')]: $1"
+    exit 1
+}
+
+function  install_ansible(){
+    apt update
+    apt install software-properties-common -y
+    apt-add-repository --yes --update ppa:ansible/ansible
+    apt update
+    apt install ansible -y
+
+    ansible --version
+}
+
+log_info "Deploying ansible resources for ${WSO2_PRODUCT_NAME}-${WSO2_PRODUCT_VERSION}"
+
+# install ansible
+log_info "Installing Ansible"
+install_ansible
+
+# Clone ansible resource repository
+log_info "Cloning ${ANSIBLE_REPO_NAME} into ${WORKING_DIR}"
+if ! git clone https://${GITUSER}:${GITPASS}@'&ANSIBLE_REPO_URL' -b '&ANSIBLE_REPO_BRANCH'; then
+    log_error "Failed to clone ${ANSIBLE_REPO_NAME}"
+fi
+log_info "Successfully cloned ${ANSIBLE_REPO_NAME}"
+
+log_info "Updating ansible resources"
+sed -i 's/DB_TYPE/${DBEngine}/g' ${WORKING_DIR}/${ANSIBLE_REPO_NAME}/roles/common/tasks/main.yml
+sed -i 's/JDK_TYPE/${JDK}/g' ${WORKING_DIR}/${ANSIBLE_REPO_NAME}/roles/common/tasks/main.yml
+sed -i 's/RDS_PWD/${DBPassword}/g' ${WORKING_DIR}/${ANSIBLE_REPO_NAME}/dev/group_vars/*
+sed -i 's/RDS_USERNAME/${DBUsername}/g' ${WORKING_DIR}/${ANSIBLE_REPO_NAME}/dev/group_vars/*
+sed -i 's/DB_ADDRESS/${DBHost}/g' ${WORKING_DIR}/${ANSIBLE_REPO_NAME}/dev/group_vars/*
+sed -i 's/SID/${SID}/g' ${WORKING_DIR}/${ANSIBLE_REPO_NAME}/dev/group_vars/*
+
+log_info "Running ansible scripts"
+cd ${ANSIBLE_REPO_NAME} 
+ansible-playbook -i ${WORKING_DIR}/${ANSIBLE_REPO_NAME}/dev ${WORKING_DIR}/${ANSIBLE_REPO_NAME}/site.yml
+
+log_info "Downloading Database provision scripts"
+if ! wget https://integration-testgrid-resources.s3.amazonaws.com/db_scripts/${DB_PROV_SCRIPT_NAME}; then
+    log_error "Downloading provision scripts failed"
+fi
+log_info "Successfully downloaded database provision scripts"
+chmod +x ${DB_PROV_SCRIPT_NAME}
+
+log_info "Updating Database provision scripts"
+sed -i 's/&CF_DB_USERNAME/${DBUsername}/g' ${DB_PROV_SCRIPT_PATH}
+sed -i 's/&CF_DB_PASSWORD/${DBPassword}/g' ${DB_PROV_SCRIPT_PATH}
+sed -i 's/&CF_DB_HOST/${DBHost}/g' ${DB_PROV_SCRIPT_PATH}
+sed -i 's/&CF_DB_PORT/${DBPort}/g' ${DB_PROV_SCRIPT_PATH}
+sed -i 's/&CF_DB_NAME/${DBEngine}/g' ${DB_PROV_SCRIPT_PATH}
+sed -i 's/&CF_DB_VERSION/${DBEngineVersion}/g' ${DB_PROV_SCRIPT_PATH}
+sed -i 's/&PRODUCT_VERSION/${WSO2_PRODUCT_VERSION}/g' ${DB_PROV_SCRIPT_PATH}
+
+bash ${DB_PROV_SCRIPT_PATH}
