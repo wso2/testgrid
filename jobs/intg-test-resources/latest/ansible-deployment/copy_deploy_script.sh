07067188abcb657a04fac782a4e41a3a236a3fcd
NishikaDeSilva <desilvanishika73@gmail.com>
2020-07-02 19:13:19 +0530

    Update scripts

--- jobs/intg-test-resources/latest/ansible-deployment/copy_deploy_script.sh
+++ jobs/intg-test-resources/latest/ansible-deployment/copy_deploy_script.sh
@@ -6,8 +6,8 @@ INPUT_DIR=$2
 
 TEST_PROP_FILE=$INPUT_DIR/testplan-props.properties
 INFRA_PROP_FILE=$INPUT_DIR/infrastructure.properties
-GIT_USER=$(grep -w "GIT_WUM_USERNAME" $TEST_P{ROP_FILE | cut -d'=' -f2)
-GIT_PASS=$(grep -w "GIT_WUM_PASSWORD" ${TEST_PROP_FILE} | cut -d'=' -f2)
+GIT_USER=$(grep -m 1 -w "GIT_WUM_USERNAME" $TEST_PROP_FILE | cut -d'=' -f2)
+GIT_PASS=$(grep -m 1 -w "GIT_WUM_PASSWORD" ${TEST_PROP_FILE} | cut -d'=' -f2)
 keyFileLocation=$(grep -w "keyFileLocation" ${TEST_PROP_FILE} | cut -d'=' -f2)
 
 OS=$(grep  -w "OS" ${INFRA_PROP_FILE} | cut -d'=' -f2)
@@ -15,18 +15,18 @@ WSO2InstanceName=$(grep -w "WSO2InstanceName" ${INFRA_PROP_FILE} | cut -d'=' -f2
 
 ANSIBLE_REPO=$(grep -w "ANSIBLE_GIT_URL" $TEST_PROP_FILE | cut -d'=' -f2 | cut -d'/' -f3-)
 ANSIBLE_BRANCH=$(grep -w "ANSIBLE_GIT_BRANCH" $TEST_PROP_FILE | cut -d'=' -f2)
-PRODUCT_NAME=$(grep -w "WSO2_PRODUCT" ${TEST_PROP_FILE}| cut -d'=' -f2 | cut -d'-' -f1)
-PRODUCT_VERSION=$(grep -w "WSO2_PRODUCT" ${TEST_PROP_FILE}| cut -d'=' -f2 | cut -d'-' -f2)
+PRODUCT_NAME=$(grep -w "ProductName" ${TEST_PROP_FILE}| cut -d'=' -f2)
+PRODUCT_VERSION=$(grep -w "ProductVersion" ${TEST_PROP_FILE}| cut -d'=' -f2)
 
 LOCAL_DEPLOYMENT_SCRIPT=${INPUT_DIR}/../workspace/InfraRepository/jobs/intg-test-resources/latest/ansible-deployment/deploy.sh
 REMOTE_DEPLOYMENT_SCRIPT=/opt/testgrid/workspace/deploy.sh
 
-sed -i "s/'&ANSIBLE_REPO_URL'/${ANSIBLE_REPO}/g" ${DEPLOYMENT_SCRIPT}
-sed -i "s/'&ANSIBLE_REPO_BRANCH'/${ANSIBLE_BRANCH}/g" ${DEPLOYMENT_SCRIPT}
+sed -i "s|ANSIBLE_REPO_URL|${ANSIBLE_REPO}|g" ${LOCAL_DEPLOYMENT_SCRIPT}
+sed -i "s|ANSIBLE_REPO_BRANCH|${ANSIBLE_BRANCH}|g" ${LOCAL_DEPLOYMENT_SCRIPT}
 
-if [ $OS = "ubuntu" ]; then
+if [ $OS = "Ubuntu" ]; then
     instanceUser="ubuntu"
 fi
 
 scp -o StrictHostKeyChecking=no -i ${keyFileLocation} ${LOCAL_DEPLOYMENT_SCRIPT} ${instanceUser}@${WSO2InstanceName}:${REMOTE_DEPLOYMENT_SCRIPT}
-ssh -o StrictHostKeyChecking=no -i ${keyFileLocation} ${instanceUser}@${WSO2InstanceName} "sudo bash ${REMOTE_DEPLOYMENT_SCRIPT} ${PRODUCT_NAME} ${PRODUCT_VERSION} ${GIT_USER} ${GIT_PASS}"
+ssh -o StrictHostKeyChecking=no -i ${keyFileLocation} ${instanceUser}@${WSO2InstanceName} "cd /opt/testgrid/workspace && sudo bash ${REMOTE_DEPLOYMENT_SCRIPT} ${PRODUCT_NAME} ${PRODUCT_VERSION} ${GIT_USER} ${GIT_PASS}"
