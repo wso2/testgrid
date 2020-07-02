8e48d2fc2764156426fdf302a150c7386100e627
NishikaDeSilva <desilvanishika73@gmail.com>
2020-07-02 19:13:19 +0530

    Update scripts

--- jobs/intg-test-resources/latest/ansible-deployment/deploy.sh
+++ jobs/intg-test-resources/latest/ansible-deployment/deploy.sh
@@ -10,7 +10,7 @@ GITPASS=$4
 WORKING_DIR=$(pwd)
 
 CFN_PROP_FILE=/opt/testgrid/workspace/cfn-props.properties
-ANSIBLE_REPO_NAME=$(echo '&ANSIBLE_REPO_URL' | rev | cut -d'/' -f1 | rev)
+ANSIBLE_REPO_NAME=$(echo ANSIBLE_REPO_URL | rev | cut -d'/' -f1 | rev)
 DB_PROV_SCRIPT_NAME="provision_db_${WSO2_PRODUCT_NAME}.sh"
 DB_PROV_SCRIPT_PATH="${WORKING_DIR}/${DB_PROV_SCRIPT_NAME}"
 
@@ -51,27 +51,27 @@ install_ansible
 
 # Clone ansible resource repository
 log_info "Cloning ${ANSIBLE_REPO_NAME} into ${WORKING_DIR}"
-if ! git clone https://${GITUSER}:${GITPASS}@'&ANSIBLE_REPO_URL' -b '&ANSIBLE_REPO_BRANCH'; then
+if ! git clone https://${GITUSER}:${GITPASS}@ANSIBLE_REPO_URL -b ANSIBLE_REPO_BRANCH; then
     log_error "Failed to clone ${ANSIBLE_REPO_NAME}"
 fi
 log_info "Successfully cloned ${ANSIBLE_REPO_NAME}"
 
 # copy product pack to ansible
-if [ -f $HOME/.wum3/products/$WSO2_PRODUCT_NAME/$WSO2_PRODUCT_VERSION/full/$WSO2_PRODUCT_NAME-$WSO2_PRODUCT_VERSION*.zip ]; then
+if [ -f /root/.wum3/products/$WSO2_PRODUCT_NAME/$WSO2_PRODUCT_VERSION/full/$WSO2_PRODUCT_NAME-$WSO2_PRODUCT_VERSION*.zip ]; then
     echo "Updated pack available"
-    cp $HOME/.wum3/products/$WSO2_PRODUCT_NAME/$WSO2_PRODUCT_VERSION/full/$WSO2_PRODUCT_NAME-$WSO2_PRODUCT_VERSION*.zip $WORKSPACE/${ANSIBLE_REPO_NAME}/files/packs/$WSO2_PRODUCT_NAME-$WSO2_PRODUCT_VERSION.zip
+    cp /root/.wum3/products/$WSO2_PRODUCT_NAME/$WSO2_PRODUCT_VERSION/full/$WSO2_PRODUCT_NAME-$WSO2_PRODUCT_VERSION*.zip ${ANSIBLE_REPO_NAME}/files/packs/$WSO2_PRODUCT_NAME-$WSO2_PRODUCT_VERSION.zip
 else
     echo "Updated pack not available. proceeding with vanilla pack"
-    cp $HOME/.wum3/products/$WSO2_PRODUCT_NAME/$WSO2_PRODUCT_VERSION/$WSO2_PRODUCT_NAME-$WSO2_PRODUCT_VERSION.zip $WORKSPACE/${ANSIBLE_REPO_NAME}/files/packs/$WSO2_PRODUCT_NAME-$WSO2_PRODUCT_VERSION.zip
+    cp /root/.wum3/products/$WSO2_PRODUCT_NAME/$WSO2_PRODUCT_VERSION/$WSO2_PRODUCT_NAME-$WSO2_PRODUCT_VERSION.zip ${ANSIBLE_REPO_NAME}/files/packs/$WSO2_PRODUCT_NAME-$WSO2_PRODUCT_VERSION.zip
 fi
 
 log_info "Updating ansible resources"
-sed -i 's/DB_TYPE/${DBEngine}/g' ${WORKING_DIR}/${ANSIBLE_REPO_NAME}/roles/common/tasks/main.yml
-sed -i 's/JDK_TYPE/${JDK}/g' ${WORKING_DIR}/${ANSIBLE_REPO_NAME}/roles/common/tasks/main.yml
-sed -i 's/RDS_PWD/${DBPassword}/g' ${WORKING_DIR}/${ANSIBLE_REPO_NAME}/dev/group_vars/*
-sed -i 's/RDS_USERNAME/${DBUsername}/g' ${WORKING_DIR}/${ANSIBLE_REPO_NAME}/dev/group_vars/*
-sed -i 's/DB_ADDRESS/${DBHost}/g' ${WORKING_DIR}/${ANSIBLE_REPO_NAME}/dev/group_vars/*
-sed -i 's/SID/${SID}/g' ${WORKING_DIR}/${ANSIBLE_REPO_NAME}/dev/group_vars/*
+sed -i "s/DB_TYPE/${DBEngine}/g" ${WORKING_DIR}/${ANSIBLE_REPO_NAME}/roles/common/tasks/main.yml
+sed -i "s/JDK_TYPE/${JDK}/g" ${WORKING_DIR}/${ANSIBLE_REPO_NAME}/roles/common/tasks/main.yml
+sed -i "s/RDS_PWD/${DBPassword}/g" ${WORKING_DIR}/${ANSIBLE_REPO_NAME}/dev/group_vars/*
+sed -i "s/RDS_USERNAME/${DBUsername}/g" ${WORKING_DIR}/${ANSIBLE_REPO_NAME}/dev/group_vars/*
+sed -i "s/DB_ADDRESS/${DBHost}/g" ${WORKING_DIR}/${ANSIBLE_REPO_NAME}/dev/group_vars/*
+sed -i "s/SID/${SID}/g" ${WORKING_DIR}/${ANSIBLE_REPO_NAME}/dev/group_vars/*
 
 log_info "Running ansible scripts"
 cd ${ANSIBLE_REPO_NAME} 
@@ -85,12 +85,12 @@ log_info "Successfully downloaded database provision scripts"
 chmod +x ${DB_PROV_SCRIPT_NAME}
 
 log_info "Updating Database provision scripts"
-sed -i 's/&CF_DB_USERNAME/${DBUsername}/g' ${DB_PROV_SCRIPT_PATH}
-sed -i 's/&CF_DB_PASSWORD/${DBPassword}/g' ${DB_PROV_SCRIPT_PATH}
-sed -i 's/&CF_DB_HOST/${DBHost}/g' ${DB_PROV_SCRIPT_PATH}
-sed -i 's/&CF_DB_PORT/${DBPort}/g' ${DB_PROV_SCRIPT_PATH}
-sed -i 's/&CF_DB_NAME/${DBEngine}/g' ${DB_PROV_SCRIPT_PATH}
-sed -i 's/&CF_DB_VERSION/${DBEngineVersion}/g' ${DB_PROV_SCRIPT_PATH}
-sed -i 's/&PRODUCT_VERSION/${WSO2_PRODUCT_VERSION}/g' ${DB_PROV_SCRIPT_PATH}
+sed -i "s/&CF_DB_USERNAME/${DBUsername}/g" ${DB_PROV_SCRIPT_NAME}
+sed -i "s/&CF_DB_PASSWORD/${DBPassword}/g" ${DB_PROV_SCRIPT_NAME}
+sed -i "s/&CF_DB_HOST/${DBHost}/g" ${DB_PROV_SCRIPT_NAME}
+sed -i "s/&CF_DB_PORT/${DBPort}/g" ${DB_PROV_SCRIPT_NAME}
+sed -i "s/&CF_DB_NAME/${DBEngine}/g" ${DB_PROV_SCRIPT_NAME}
+sed -i "s/&CF_DB_VERSION/${DBEngineVersion}/g" ${DB_PROV_SCRIPT_NAME}
+sed -i "s/&PRODUCT_VERSION/${WSO2_PRODUCT_VERSION}/g" ${DB_PROV_SCRIPT_NAME}
 
-bash ${DB_PROV_SCRIPT_PATH}
+bash ${DB_PROV_SCRIPT_NAME}
