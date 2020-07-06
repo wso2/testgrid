8727751696afa0d4cf8d85a02667aaaba7651f3a
NishikaDeSilva <desilvanishika73@gmail.com>
2020-07-06 13:54:59 +0530

    Add License header

--- jobs/intg-test-resources/latest/ansible-deployment/deploy.sh
+++ jobs/intg-test-resources/latest/ansible-deployment/deploy.sh
@@ -1,19 +1,31 @@
+#----------------------------------------------------------------------------
+#  Copyright (c) 2020 WSO2, Inc. http://www.wso2.org
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#----------------------------------------------------------------------------
 #!/bin/bash
 
 set -o xtrace
 
+WORKING_DIR=$(pwd)
 WSO2_PRODUCT_NAME=$1
 WSO2_PRODUCT_VERSION=$2
 GITUSER=$3
 GITPASS=$4
-
-WORKING_DIR=$(pwd)
-
 CFN_PROP_FILE=/opt/testgrid/workspace/cfn-props.properties
 ANSIBLE_REPO_NAME=$(echo ANSIBLE_REPO_URL | rev | cut -d'/' -f1 | rev)
 DB_PROV_SCRIPT_NAME="provision_db_${WSO2_PRODUCT_NAME}.sh"
 DB_PROV_SCRIPT_PATH="${WORKING_DIR}/${DB_PROV_SCRIPT_NAME}"
-
 # get aws properties
 DBEngine=$(grep -w "DB_TYPE" ${CFN_PROP_FILE} | cut -d'=' -f2)
 DBEngineVersion=$(grep -w "CF_DB_VERSION" ${CFN_PROP_FILE} | cut -d'=' -f2)
