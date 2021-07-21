d2172694814a4f09e2890af7258560e953e94e62
Tharindu Dharmarathna <tharindua@wso2.com>
2021-07-22 00:10:46 +0530

    bring uat-setting into s3

--- jobs/intg-test-resources/update2-releases/run-test.sh
+++ jobs/intg-test-resources/update2-releases/run-test.sh
@@ -34,8 +34,6 @@ WUM_USERNAME=$(grep -w "WUM_USERNAME" ${CONFIG_PROP_FILE} | cut -d'=' -f2)
 WUM_PASSWORD=$(grep -w "WUM_PASSWORD" ${CONFIG_PROP_FILE} | cut -d'=' -f2)
 SCRIPT_LOCATION=$(grep -w "TEST_SCRIPT_URL" ${PROP_FILE} | cut -d'=' -f2)
 TEST_SCRIPT_NAME=$(echo $SCRIPT_LOCATION | rev | cut -d'/' -f1 | rev)
-NEXUS_SCRIPT_NAME="uat-nexus-settings.xml"
-NEXUS_SCRIPT_PATH="/testgrid/testgrid-home/jobs/u2-intg-test-${PRODUCT_NAME}-${PRODUCT_VERSION}-full"
 INFRA_JSON=$INPUTS_DIR/../workspace/InfraRepository/jobs/intg-test-resources/infra.json
 GIT_USER=$(grep -w "GIT_WUM_USERNAME" ${PROP_FILE} | cut -d'=' -f2)
 GIT_PASS=$(grep -w "GIT_WUM_PASSWORD" ${PROP_FILE} | cut -d'=' -f2)
@@ -57,7 +55,6 @@ else
 fi
 
 scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i ${keyFileLocation} ${TEST_SCRIPT_NAME} $instanceUser@${WSO2InstanceName}:/opt/testgrid/workspace/${TEST_SCRIPT_NAME}
-scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i ${keyFileLocation} ${NEXUS_SCRIPT_PATH}/${NEXUS_SCRIPT_NAME} $instanceUser@${WSO2InstanceName}:/opt/testgrid/workspace/${NEXUS_SCRIPT_NAME}
 scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i ${keyFileLocation} ${INFRA_JSON} $instanceUser@${WSO2InstanceName}:/opt/testgrid/workspace/infra.json
 ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i ${keyFileLocation} $instanceUser@${WSO2InstanceName} "cd /opt/testgrid/workspace && sudo bash /opt/testgrid/workspace/wso2-update.sh" "'$WUM_USERNAME'" "'$WUM_PASSWORD'"
 ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i ${keyFileLocation} $instanceUser@${WSO2InstanceName} "cd /opt/testgrid/workspace && sudo bash /opt/testgrid/workspace/provision_db_${PRODUCT_NAME}.sh"
